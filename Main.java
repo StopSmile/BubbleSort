a250d4dbe807ca5285618775f6fe230cd80b41c4
Ivan Pylypiv <38442142+StopSmile@users.noreply.github.com>
2020-08-07 22:47:01 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[]array = {10,3,5,6,1,-2,-10};
+        SortAlgorithm BubbleSort = new SortAlgorithm();
+        BubbleSort.BubbleSort(array);
+
+
+    }
+}
---
+++ SortAlgorithm.java
@@ -0,0 +1,22 @@
+public class SortAlgorithm {
+    public void BubbleSort(int[] mas) {
+        boolean x = false;
+        int buf;
+        while (!x) {
+            x = true;
+            for (int i = 0; i < mas.length - 1; i++) {
+                if (mas[i] > mas[i + 1]) {
+                    x = false;
+
+                    buf = mas[i];
+                    mas[i] = mas[i + 1];
+                    mas[i + 1] = buf;
+                }
+            }
+        }
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print(mas[i] + " ");
+        }
+
+    }
+}
