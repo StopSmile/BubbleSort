5f4c4e7c819dbb333ed8867a2cce25aa46dc3d73
stopsmile1@gmail.com <Forward1234567890>
2021-03-05 22:17:13 +0200

    backup

--- SortAlgorithm.java
+++ SortAlgorithm.java
@@ -16,7 +16,6 @@ public class SortAlgorithm {
         }
         for (int i = 0; i < mas.length; i++) {
             System.out.print(mas[i] + " ");
-            System.out.println("Hello World");
         }
 
     }
